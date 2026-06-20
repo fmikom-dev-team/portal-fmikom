01cf7d52983285ec9cae292a56f7a921407410d4
FMIKOM Dev Team <fmikom.dev@gmail.com>
2026-06-20 22:38:15 +0700

    fix: hapus Telescope dari providers, daftarkan kondisional & gitignore bootstrap/cache

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,11 +3,9 @@
 use App\Providers\AppServiceProvider;
 use App\Providers\FortifyServiceProvider;
 use App\Providers\HorizonServiceProvider;
-use App\Providers\TelescopeServiceProvider;
 
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
     HorizonServiceProvider::class,
-    TelescopeServiceProvider::class,
 ];
