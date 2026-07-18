fee330869889f66e04c75660e9c564b7d8e92a6b
FMIKOM Dev Team <fmikom.dev@gmail.com>
2026-07-19 00:59:45 +0700

    fix: import Log facade in HandlesImageCompression to resolve PHPStan unknown class error

--- app/Concerns/HandlesImageCompression.php
+++ app/Concerns/HandlesImageCompression.php
@@ -6,6 +6,7 @@ use App\Jobs\OptimizeVideoJob;
 use App\Services\VirusScannerService;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 
 trait HandlesImageCompression
